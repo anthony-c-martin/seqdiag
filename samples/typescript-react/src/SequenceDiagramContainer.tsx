63782c4012ee74278481ab3d17969263d74da1f1
Anthony Martin <anthony.ct.martin@gmail.com>
2020-12-06 11:42:24 -0800

    Use React.FC in example

--- samples/typescript-react/src/SequenceDiagramContainer.tsx
+++ samples/typescript-react/src/SequenceDiagramContainer.tsx
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { renderSequenceDiagram, SequenceDiagramProps } from 'seqdiag';
 
-export class SequenceDiagramContainer extends React.Component<{data: SequenceDiagramProps}, {}> {
-  containerRef: React.RefObject<HTMLDivElement>;
-  constructor(props) {
-    super(props);
-    this.containerRef = React.createRef();
+type Props = {
+  data: SequenceDiagramProps;
+}
+
+export const SequenceDiagramContainer: React.FC<Props> = (props) => {
+  const containerRef = useRef<HTMLDivElement>();
+  const { data } = props;
+
+  // initial render
+  useEffect(() => renderSequenceDiagram(containerRef.current, data), []);
+
+  //subsequent renders
+  if (containerRef.current) {
+    renderSequenceDiagram(containerRef.current, data);
   }
 
-  componentDidMount() {
-    renderSequenceDiagram(this.containerRef.current, this.props.data);
-  }
+  return (
+    <>
+      <div ref={containerRef}></div>
+      <button onClick={() => downloadSvg(containerRef.current)}>Download</button>
+    </>
+  );
+};
 
-  downloadSvg() {
-    const svgData = '<?xml version="1.0" encoding="UTF-8"?>\r\n' + this.containerRef.current.innerHTML;
-  
-    const data = new Blob([svgData], { type: 'image/svg+xml;charset=utf-8' });
-    const csvURL = window.URL.createObjectURL(data);
-    const tempLink = document.createElement('a');
-    tempLink.href = csvURL;
-    tempLink.setAttribute('download', `diagram.svg`);
-    tempLink.click();
-  }
+function downloadSvg(container: HTMLDivElement) {
+  const svgData = '<?xml version="1.0" encoding="UTF-8"?>\r\n' + container.innerHTML;
 
-  render() {
-    return (
-      <>
-        <div ref={this.containerRef}></div>
-        <button onClick={this.downloadSvg.bind(this)}>Download</button>
-      </>
-    );
-  }
+  const data = new Blob([svgData], { type: 'image/svg+xml;charset=utf-8' });
+  const csvURL = window.URL.createObjectURL(data);
+  const tempLink = document.createElement('a');
+  tempLink.href = csvURL;
+  tempLink.setAttribute('download', `diagram.svg`);
+  tempLink.click();
 }
